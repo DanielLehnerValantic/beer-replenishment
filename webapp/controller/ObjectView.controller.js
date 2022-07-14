1922ac5090d7da0facca0724b3704d4082d10d65
DanielLehnerValantic <daniel.lehner@sce.valantic.com>
2022-07-14 19:50:26 +0000

    Anpassungen Views

--- webapp/controller/ObjectView.controller.js
+++ webapp/controller/ObjectView.controller.js
@@ -1,107 +1,86 @@
 sap.ui.define([
 	"./BaseController",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/core/routing/History",
-	"mycompany/myapp/MyWorklistApp/model/formatter",
-	"sap/ui/core/format/DateFormat",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function (
-	BaseController, JSONModel, History, formatter, DateFormat, Filter, FilterOperator) {
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/m/Dialog",
+	"sap/m/Button",
+    "sap/m/MessageToast",
+	"sap/m/Text"
+], function (BaseController, Filter, FilterOperator, MessageToast, Text, Dialog, Button) {
 	"use strict";
 
 	return BaseController.extend("beerreplenishment.controller.ObjectView", {
 
-		formatter: formatter,
-
-		/* =========================================================== */
-		/* lifecycle methods                                           */
-		/* =========================================================== */
-
-		/**
-		 * Called when the worklist controller is instantiated.
-		 * @public
-		 */
 		onInit : function () {
-            console.log("22222");
+            this.getRouter().getRoute("ObjectView").attachMatched(this.onRouteMatched, this);
+		},
 
-            // var oViewModel = new JSONModel({
-            //     worklistTableTitle: this.getResourceBundle().getText("worklistTableTitle"),
-            //     tableBusyDelay: 0,
-            // });
-            // this.setModel(oViewModel, "mainviewView");
+        onRouteMatched: function(oEvent) {
 
-            // var oData = sap.ui.getCore().getModel("modelName").getData();
-            // console.log("213123213");
-            // console.log(oData);
-			// Model used to manipulate control states. The chosen values make sure,
-			// detail page is busy indication immediately so there is no break in
-			// between the busy indication for loading the view's meta data
-			var iOriginalBusyDelay,
-				oViewModel = new JSONModel({
-					busy : true,
-					delay : 0
+            // get selected item
+            var oArguments = oEvent.getParameter("arguments");
+            var Lgtyp = oArguments.Lgtyp;
+
+            // filter for selected item
+            var aFilter = [];
+            var sQuery = Lgtyp;
+
+            if (sQuery) {
+                aFilter.push(new Filter("Lgtyp", FilterOperator.Contains, sQuery));
+            }
+
+            // filter binding
+            var oList = this.getView().byId("productTable");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
+
+
+
+            
+        },
+
+        onSearchProduct: function(oEvent) {
+            var aFilter = [];
+            var sQuery = oEvent.getParameter("query");
+
+            if (sQuery) {
+                aFilter.push(new Filter("Lgtyp", FilterOperator.Contains, sQuery));
+            }
+
+            // filter binding
+            var oList = this.getView().byId("productTable");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
+        },
+
+        openDialog: function(oEvent) {
+            console.log("test");
+        },
+
+        onApproveDialogPress: function () {
+			if (!this.oApproveDialog) {
+				this.oApproveDialog = new Dialog({
+					type: DialogType.Message,
+					title: "Confirm",
+					content: new Text({ text: "Do you want to submit this order?" }),
+					beginButton: new Button({
+						type: ButtonType.Emphasized,
+						text: "Submit",
+						press: function () {
+							MessageToast.show("Submit pressed!");
+							this.oApproveDialog.close();
+						}.bind(this)
+					}),
+					endButton: new Button({
+						text: "Cancel",
+						press: function () {
+							this.oApproveDialog.close();
+						}.bind(this)
+					})
 				});
-
-                var oData = sap.ui.getCore().getModel("modelname").getData();
-                console.log(oData);
-
-			this.getRouter().getRoute("objectview").attachPatternMatched(this._onObjectMatched, this);
-
-			// Store original busy indicator delay, so it can be restored later on
-			iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
-			this.setModel(oViewModel, "objectviewView");
-			this.getOwnerComponent().getModel().metadataLoaded().then(function () {
-					// Restore original busy indicator delay for the object view
-					oViewModel.setProperty("/delay", iOriginalBusyDelay);
-				}
-			);
-		},
-
-        _onPostMatched: function (oEvent) {
-			var oViewModel = this.getModel("objectview"),
-				oDataModel = this.getModel();
-                console.log(oDataModel);
-                console.log("test123");
-
-			this.getView().bindElement({
-				path: "/ZEWMIEFSI01STSet('" + oEvent.getParameter("arguments").postId + "')",
-				events: {
-					dataRequested: function () {
-						oDataModel.metadataLoaded().then(function () {
-							// Busy indicator on view should only be set if metadata is loaded,
-							// otherwise there may be two busy indications next to each other on the
-							// screen. This happens because route matched handler already calls '_bindView'
-							// while metadata is loaded.
-							oViewModel.setProperty("/busy", true);
-						});
-					},
-					dataReceived: function () {
-						oViewModel.setProperty("/busy", false);
-					}
-				}
-			});
-		},
-
-		/* =========================================================== */
-		/* event handlers                                              */
-		/* =========================================================== */
-
-
-		/**
-		 * Event handler  for navigating back.
-		 * It there is a history entry we go one step back in the browser history
-		 * If not, it will replace the current entry of the browser history with the worklist route.
-		 * @public
-		 */
-		onNavBack : function() {
-			var sPreviousHash = History.getInstance().getPreviousHash();
-
-			if (sPreviousHash !== undefined) {
-				history.go(-1);
-			} else {
-				this.getRouter().navTo("mainview", {}, true);
 			}
+
+			this.oApproveDialog.open();
 		}
 
 	});
