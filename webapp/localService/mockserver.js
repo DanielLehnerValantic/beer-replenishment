da1fec9ab5025fb609198c5a1f525321bcf10368
DanielLehnerValantic <daniel.lehner@sce.valantic.com>
2022-07-25 13:40:05 +0000

    aufsetzen mockserver

---
+++ webapp/localService/mockserver.js
@@ -0,0 +1,33 @@
+sap.ui.define([
+	"sap/ui/core/util/MockServer"
+], function(MockServer) {
+	"use strict";
+
+	return {
+		/**
+		 * Initializes the mock server.
+		 * You can configure the delay with the URL parameter "serverDelay".
+		 * The local mock data in this folder is returned instead of the real data for testing.
+		 * @public
+		 */
+		init: function() {
+
+			this.oMockServer = new MockServer({
+				rootUri: "/sap/opu/odata/sap/ZEWM_EF_SI01_SRV/"
+			});
+			var sMetadataRelativePath = "../localService/metadata.xml";
+			var oMockSettings = {
+				sMockdataBaseUrl: "../localService/data",
+				bGenerateMissingMockData: false
+			};
+			var oMockServer = this.oMockServer;
+
+			oMockServer.simulate(sMetadataRelativePath, oMockSettings);
+			oMockServer.start();
+
+            console.log("mockserver started");
+		}
+
+	};
+
+});
